df178bd2f5e0851482ca185b58ba09d54ec50f73
Learner625@gmail.com <studylearning592@gmail.com>
2021-04-11 15:53:54 +0600

    t

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-      function getInputValue() {
+
+
+       document.querySelector(".indexlogin").addEventListener('click',function(){
         // Selecting the input element and get its value 
         let inputVal = document.querySelector(".netlify-identity-user").value;
         // Displaying the value
         document.querySelector(".username").innerHTML=inputval;     
-       }
-
-       document.querySelector(".indexlogin").addEventListener('click',getInputValue());
+       });
