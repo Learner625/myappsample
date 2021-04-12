dafb830cd456e34b2ead8be0ac9393c736c7fdde
Learner625@gmail.com <studylearning592@gmail.com>
2021-04-12 06:24:21 +0600

    t

--- app.js
+++ app.js
@@ -4,5 +4,7 @@
         // Selecting the input element and get its value 
         const inputVal = document.querySelector('.netlify-identity-user').value;
         // Displaying the value
+        window.location.replace("login.html");
+
         document.querySelector('.username').innerHTML=inputVal;     
        });
