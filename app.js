f6fe7cdd51fde0335d127f27816b2c875bb0f325
Learner625@gmail.com <studylearning592@gmail.com>
2021-04-12 06:47:42 +0600

    i

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 function n1(){
-       document.querySelector('.indexlogin').addEventListener('click',function(){
+       // document.querySelector('.netlify-identity-logout').addEventListener('click',function(){
         // Selecting the input element and get its value 
-        const inputVal = document.querySelector('.netlify-identity-user').value;
+       //  const inputVal = document.querySelector('.netlify-identity-user').value;
         // Displaying the value
         window.location.assign("login.html");
 
-        document.querySelector('.username').innerHTML=inputVal;     
-       });
+       //  document.querySelector('.username').innerHTML=inputVal;     
+       // }
+       // );
 }
