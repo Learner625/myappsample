7a6740601965e194cbf3ac95f9a868fa5c441391
Learner625@gmail.com <studylearning592@gmail.com>
2021-04-12 23:32:38 +0600

    t

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 
-function n1(){
+function n2(){
        // document.querySelector('.netlify-identity-logout').addEventListener('click',function(){
         // Selecting the input element and get its value 
        //  const inputVal = document.querySelector('.netlify-identity-user').value;
         // Displaying the value
-        window.location.assign("login.html");
-
+       //  window.location.assign("login.html");
+       // document.querySelector('.btn').innerHTML="<a onclick='n2()' href='login.html'>Login</a>"
+       // ; 
+       document.querySelector('.btn').load=window.location.assign('login.html')
+       ;
+// function n2(){
+//        window.location.assign("login.html");
+// }
+       //  window.onload = function() {
+       //        var anchors = document.querySelector('.netlify-identity-logout');
+       //        for(var i = 0; i < anchors.length; i++) {
+       //            var anchor = anchors[i];
+       //            anchor.onclick = function() {
+       //                alert('ho ho ho');
+       //            }
+       //        }
+       //    }
        //  document.querySelector('.username').innerHTML=inputVal;     
        // }
        // );
